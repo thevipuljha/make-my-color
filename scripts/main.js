b09c2516969d88d9761fff50d4d0e94824dbaeb2
Vipul Jha <vipuljha1142@gmail.com>
2020-06-07 23:52:09 +0530

    one slider is changing only one value now

--- scripts/main.js
+++ scripts/main.js
@@ -1,43 +1,31 @@
-const element = (id) => document.getElementById(id);
+const elementById = (id) => document.getElementById(id);
+const elementsByclass = (className) => document.getElementsByClassName(className);
 
-const changeColorsOnBox = () => {
-    const red = element("redSlider").value;
-    const green = element("greenSlider").value;
-    const blue = element("blueSlider").value;
-    const alpha = element("alphaSlider").value;
+const changeBoxColor = (colorSliders) => {
+    const red = colorSliders[0].value;
+    const green = colorSliders[1].value;
+    const blue = colorSliders[2].value;
+    const alpha = colorSliders[3].value;
     const color = `rgba(${red},${green},${blue},${alpha/100})`;
-    element("colorWindow").style.backgroundColor = color;
-    element("color1").style.backgroundColor = color;
+    elementById("colorWindow").style.backgroundColor = color;
 }
-const setColorSliderValue = () => {
-    element("redSlider").value = element("redInput").value;
-    element("greenSlider").value = element("greenInput").value;
-    element("blueSlider").value = element("blueInput").value;
-    element("alphaSlider").value = element("alphaInput").value;
-};
-const setColorInputValue = () => {
-    element("redInput").value = element("redSlider").value;
-    element("greenInput").value = element("greenSlider").value;
-    element("blueInput").value = element("blueSlider").value;
-    element("alphaInput").value = element("alphaSlider").value;
-};
-const addEventListeners = () => {
-    const colorSliders = ["redSlider", "greenSlider", "blueSlider", "alphaSlider"];
-    const colorInputs = ["redInput", "greenInput", "blueInput", "alphaInput"];
-    colorSliders.forEach(id => {
-        element(id).addEventListener('input', event => {
-            setColorInputValue();
-            changeColorsOnBox();
+
+const addEventListeners = (colorSliders, colorInputs) => {
+    for (let index = 0; index < 4; index++) {
+        colorSliders[index].addEventListener('input', () => {
+            colorInputs[index].value = colorSliders[index].value;
+            changeBoxColor(colorSliders);
         });
-    });
-    colorInputs.forEach(id => {
-        element(id).addEventListener('change', event => {
-            setColorSliderValue();
-            changeColorsOnBox();
+        colorInputs[index].addEventListener('change', () => {
+            colorSliders[index].value = colorInputs[index].value;
+            changeBoxColor(colorSliders);
         });
-    });
+    }
 }
+
 const initiate = () => {
-    addEventListeners();
+    const colorSliders = elementsByclass("color-slider");
+    const colorInputs = elementsByclass("color-input");
+    addEventListeners(colorSliders, colorInputs);
 };
 window.onload = initiate;
